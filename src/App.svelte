9c3922de2091e55b4270ce901e1fb5cbba1c5a82
FlachaireNathan <nathan.flachaire@outlook.fr>
2022-01-22 23:32:20 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+import Router, {location, link} from "svelte-spa-router";
+import Menu from "./components/page/Menu.svelte";
+</script>
+
+<Router routes={{
+	'/': Menu,
+	'/menu': Menu,
+}}></Router>
